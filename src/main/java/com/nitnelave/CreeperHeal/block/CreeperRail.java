701a5a1495e691c1fd2b82ddc71561f305222e22
azure_crystal_ <stillcrystal@outlook.jp>
2017-12-20 20:10:35 +0900

    BlockState#data プロパティへのアクセス方法を変更
    fix #1
    setData() は謎の型検査が働いてスタックトレースをぶち撒けるので、リフレクションで直接アクセスするように変更

--- src/main/java/com/nitnelave/CreeperHeal/block/CreeperRail.java
+++ src/main/java/com/nitnelave/CreeperHeal/block/CreeperRail.java
@@ -1,19 +1,21 @@
 package com.nitnelave.CreeperHeal.block;
 
-import com.nitnelave.CreeperHeal.utils.CreeperUtils;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
 import org.bukkit.material.Rails;
 
-import java.util.Set;
+import com.nitnelave.CreeperHeal.utils.CreeperUtils;
 
 /**
  * Rail implementation of CreeperBlock.
- * 
+ *
  * @author nitnelave
- * 
+ *
  */
 class CreeperRail extends CreeperBlock {
   private static final BlockFace[] FACES = { BlockFace.EAST, BlockFace.WEST, BlockFace.NORTH,
@@ -37,12 +39,11 @@ class CreeperRail extends CreeperBlock {
 
   /*
    * (non-Javadoc)
-   * 
+   *
    * @see com.nitnelave.CreeperHeal.block.CreeperBlock#update()
    */
   @Override
   public void update() {
-
     Rails[][] railData = new Rails[3][4];
     Block block = getBlock();
     for (int i = 0; i < 3; i++) {
@@ -54,7 +55,26 @@ class CreeperRail extends CreeperBlock {
       }
     }
     super.update();
-    block.getState().setData(data);
+
+    java.lang.reflect.Field theDataField = stateData.get();
+    if (theDataField == null) {
+      try {
+        theDataField = block.getState().getClass().getDeclaredField("data");
+        theDataField.setAccessible(true);
+      } catch (Exception exception) {
+        logger.fatal("", exception);
+        return;
+      }
+      stateData.compareAndSet(null, theDataField);
+    }
+
+    try {
+      theDataField.set(block.getState(), data);
+    } catch (Exception exception) {
+      logger.catching(exception);
+      block.getState().setData(data);
+    }
+
     for (int i = 0; i < 3; i++) {
       Block upBlock = block.getRelative(UP_DOWN[i]);
       for (int j = 0; j < 4; j++) {
@@ -63,15 +83,20 @@ class CreeperRail extends CreeperBlock {
           tmpBlock.getState().setData(railData[i][j]);
       }
     }
+
   }
+
+  private static final org.apache.logging.log4j.Logger logger = org.apache.logging.log4j.LogManager.getLogger(CreeperRail.class);
+  private static final AtomicReference<java.lang.reflect.Field> stateData = new AtomicReference<java.lang.reflect.Field>();
 
   /*
    * (non-Javadoc)
-   * 
+   *
    * @see com.nitnelave.CreeperHeal.block.CreeperBlock#getAttachingFace()
    */
   @Override
   public BlockFace getAttachingFace() {
+    Rails data = this.data;
     if (data.isOnSlope())
       return data.getDirection();
     return BlockFace.DOWN;
@@ -79,10 +104,11 @@ class CreeperRail extends CreeperBlock {
 
   /**
    * Checks if the rail is ascending.
-   * 
+   *
    * @return True, if it is ascending
    */
   public boolean isAscending() {
     return data.isOnSlope();
   }
+
 }
